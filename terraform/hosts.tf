bb159a959caa1235cd33418a6f9c044afaf4e6ec
Matthew Booth <mbooth@redhat.com>
2023-06-05 21:42:39 +0100

    Add host extra disks

--- terraform/hosts.tf
+++ terraform/hosts.tf
@@ -88,3 +88,35 @@ resource "azurerm_virtual_machine_data_disk_attachment" "hosts_nova" {
   lun                = "10"
   caching            = "None"
 }
+
+# Each host has some number of extra disks specified in hosts.extra_disks
+locals {
+  extra_disks = flatten([
+    for host_i, host in var.hosts : [
+      for extra_disk_i, extra_disk in host.extra_disks : {
+        host = host_i
+        disk = extra_disk_i
+        size = extra_disk
+      }
+    ]
+  ])
+}
+
+resource "azurerm_managed_disk" "hosts_extra" {
+  count               = length(local.extra_disks)
+  name                = format("host-%02d-extra-%02d", local.extra_disks[count.index].host, local.extra_disks[count.index].disk)
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = local.extra_disks[count.index].size
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "hosts_extra" {
+  count              = length(local.extra_disks)
+  managed_disk_id    = azurerm_managed_disk.hosts_extra[count.index].id
+  virtual_machine_id = azurerm_linux_virtual_machine.hosts[local.extra_disks[count.index].host].id
+  lun                = format("%d", 11 + local.extra_disks[count.index].disk)
+  caching            = "None"
+}
